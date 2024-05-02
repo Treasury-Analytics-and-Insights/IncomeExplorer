51bc8352633273f4c5e901207c750af76057eb8f
Luke Symes <Luke.Symes@treasury.govt.nz>
2024-05-03 10:34:33 +1200

    When calculating y_min for income composition plots, use the min function instead of the max function, as the tax values are negative.

--- server.R
+++ server.R
@@ -228,7 +228,7 @@ shinyServer(function(input, output, session) {
       output[[plot_id]] <- renderPlotly({
         X_results <- req(get_scenario_incomes())
         if (nrow(X_results) > 0) {
-          y_min <- 52*max(X_results[, wage_tax_and_ACC + benefit_tax])
+          y_min <- 52*min(X_results[, wage_tax_and_ACC + benefit_tax])
           y_max <- 52*max(X_results[, Net_Income])
           output_plot <- income_composition_plot(
             X_results[Scenario == scenario], y_min = y_min, y_max = y_max
