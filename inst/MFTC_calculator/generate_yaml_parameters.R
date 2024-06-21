bdbb024c30b467b8d6d272ca2a8753b331dde29b
Luke Symes <Luke.Symes@treasury.govt.nz>
2024-06-21 14:32:01 +1200

    Add BEFU24 params & script to convert TAWA yaml params

---
+++ inst/MFTC_calculator/generate_yaml_parameters.R
@@ -0,0 +1,15 @@
+library(data.table)
+library(magrittr)
+library(glue)
+
+efu <- "BEFU24"
+param_dir <- glue("inst/tawa_parameters")
+param_paths <- list.files(param_dir, pattern = glob2rx("*.yaml"), full.names = TRUE)
+for (param_path in param_paths) {
+  params <- IncomeExplorer::parameters_from_file(param_path)
+  yaml_param_filename <- basename(param_path) %>%
+    tools::file_path_sans_ext() %>%
+    stringr::str_remove(paste0("_", efu)) %>%
+    {paste0(efu, "_", ., ".yaml")}
+  yaml::write_yaml(params, file.path("inst/parameters", yaml_param_filename))
+}
