a327b3c7cfc57e4a3bd3518687392ff9f348c39c
Luke Symes <Luke.Symes@treasury.govt.nz>
2025-01-09 15:24:25 +1300

    Tidy up the generate_parameters script, delete some old functions which are no longer used.

--- inst/MFTC_calculator/generate_parameters.R
+++ inst/MFTC_calculator/generate_parameters.R
@@ -1,21 +1,23 @@
-# this is to be used and run with each new EFU
+# Creates app parameter yaml files from TAWA yaml parameters.
+# To be used and run with each new EFU once it is publicly available.
 
-# Split up a TAWA parameters file into several TAWA parameter files,
-# one for each Tax Year
-
-# before running: set working directory to MFTC_calculator and create a folder
-# named TAWA_Parameters
-
-IncomeExplorer::split_TAWA_parameters(
-  params_path = "",
-  output_suffix = "",
-  output_dir = "TAWA_Parameters"
+tawa_param_paths <- c(
+  "inst/TY25_HYEFU24.yaml",
+  "inst/TY26_HYEFU24.yaml",
+  "inst/TY27_HYEFU24.yaml",
+  "inst/TY28_HYEFU24.yaml",
+  "inst/TY29_HYEFU24.yaml"
 )
-
-# Convert each TAWA parameter file to IncomeExplorer App format
-IncomeExplorer::TAWA_to_app_param_files(
-  input_param_paths = list.files("TAWA_Parameters/", full.names = TRUE),
-  output_param_dir = "App_Parameters/",
-  output_prefix = "IncomeExplorer_",
-  output_template_path = "App_Parameters_Template.xlsx"
+output_paths <- c(
+  "inst/parameters/TY25_HYEFU24.yaml",
+  "inst/parameters/TY26_HYEFU24.yaml",
+  "inst/parameters/TY27_HYEFU24.yaml",
+  "inst/parameters/TY28_HYEFU24.yaml",
+  "inst/parameters/TY29_HYEFU24.yaml"
 )
+for (ii in seq_along(tawa_param_paths)) {
+  tawa_param_path <- tawa_param_paths[[ii]]
+  output_path <- output_paths[[ii]]
+  params <- IncomeExplorer::parameters_from_file(tawa_param_path)
+  yaml::write_yaml(params, output_path)
+}
