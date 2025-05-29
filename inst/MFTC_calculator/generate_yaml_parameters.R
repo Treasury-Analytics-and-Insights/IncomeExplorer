8c1657dd93e6f89674ee791e8d060de4e30317d6
Luke Symes <Luke.Symes@treasury.govt.nz>
2025-05-29 12:55:46 +1200

    Update parameters to BEFU25 (post-budget). TY25 and TY26 are now historical.

--- inst/MFTC_calculator/generate_yaml_parameters.R
+++ inst/MFTC_calculator/generate_yaml_parameters.R
@@ -2,8 +2,8 @@ library(data.table)
 library(magrittr)
 library(glue)
 
-efu <- "BEFU24"
-param_dir <- glue("inst/tawa_parameters")
+efu <- "BEFU25"
+param_dir <- glue("inst/BEFU25-TAWA/")
 param_paths <- list.files(param_dir, pattern = glob2rx("*.yaml"), full.names = TRUE)
 for (param_path in param_paths) {
   params <- IncomeExplorer::parameters_from_file(param_path)
@@ -11,5 +11,5 @@ for (param_path in param_paths) {
     tools::file_path_sans_ext() %>%
     stringr::str_remove(paste0("_", efu)) %>%
     {paste0(., "_", efu, ".yaml")}
-  yaml::write_yaml(params, file.path("inst/parameters", yaml_param_filename))
+  yaml::write_yaml(params, file.path("inst/BEFU25-app", yaml_param_filename))
 }
